f7870c702408679ad9b1018c62c9e5319f0dc73b
gikkman <ksr.jonasson@gmail.com>
2020-03-09 22:10:32 +0100

    Styling and such

--- src-client/index.ts
+++ src-client/index.ts
@@ -1,6 +1,7 @@
 import { getFileContent } from "./file-repository";
 import { updateContent } from "./content-update-service";
 
+setVariablesFromImages();
 setInterval( () => {
     getFileContent( apply )
 }, 1000);
@@ -9,4 +10,14 @@ function apply(err?: Error, data?: string): void {
     if(err) return updateContent(err.message);
     else if(data) return updateContent(data);
     else updateContent("~ No content ~");
+}
+
+function setVariablesFromImages() {
+    const img = new Image();
+    img.onload = () => {
+        const root = document.documentElement;
+        root.style.setProperty('--width', img.width + "px");
+        root.style.setProperty('--height', img.height + "px");
+    }
+    img.src = "/border-left.png";
 }
